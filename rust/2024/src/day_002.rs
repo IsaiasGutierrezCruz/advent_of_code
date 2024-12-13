aa7559f9e1e2311391132cc917aa53794827fb8a
Isaias Gutierrez Cruz <isaiasgutierrezqfb@gmail.com>
2024-12-12 20:56:07 -0600

    feat: completed day 2

--- rust/2024/src/day_002.rs
+++ rust/2024/src/day_002.rs
@@ -67,6 +67,72 @@ pub fn solve_part_01(input: &Vec<Vec<usize>>) -> usize {
     safe_reports
 }
 
+// --- Part Two ---
+// The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+// The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+// Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+// More of the above example's reports are now safe:
+
+// 7 6 4 2 1: Safe without removing any level.
+// 1 2 7 8 9: Unsafe regardless of which level is removed.
+// 9 7 6 2 1: Unsafe regardless of which level is removed.
+// 1 3 2 4 5: Safe by removing the second level, 3.
+// 8 6 4 4 1: Safe by removing the third level, 4.
+// 1 3 6 7 9: Safe without removing any level.
+// Thanks to the Problem Dampener, 4 reports are actually safe!
+
+// Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+
+fn unsafe_indexes(report: &Vec<usize>) -> Vec<usize> {
+    let mut index = 1;
+    let is_decreasing: bool = report[0] > report[1];
+    let mut bad_level_indexes: Vec<usize> = Vec::new();
+
+    while index < report.len() {
+        let diff = if is_decreasing {
+            report[index - 1].checked_sub(report[index])
+        } else {
+            report[index].checked_sub(report[index - 1])
+        };
+
+        match diff {
+            Some(d) if d >= 1 && d <= 3 => index += 1,
+            _ => {
+                bad_level_indexes.push(index);
+                bad_level_indexes.push(index - 1);
+                index += 1;
+            },
+        }
+    }
+    bad_level_indexes
+}
+
+#[aoc(day2, part2)]
+pub fn solve_part_02(input: &Vec<Vec<usize>>) -> usize {
+    let reports = input.clone();
+    let mut safe_reports = 0;
+
+    for report in reports {
+        let bad_indexes = unsafe_indexes(&report);
+        if bad_indexes.is_empty() {
+            safe_reports += 1;
+        } else {
+            for index in bad_indexes {
+                let mut new_report = report.clone();
+                new_report.remove(index);
+                if unsafe_indexes(&new_report).is_empty() {
+                    safe_reports += 1;
+                    break;
+                }
+            }
+        }
+    }
+    safe_reports
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,8 +149,8 @@ mod tests {
         assert_eq!(solve_part_01(&input_generator(DATA)), 2)
     }
 
-    // #[test]
-    // fn sample_02() {
-    //     assert_eq!(solve_part_02(&input_generator(DATA)), 31)
-    // }
+    #[test]
+    fn sample_02() {
+        assert_eq!(solve_part_02(&input_generator(DATA)), 4)
+    }
 }
