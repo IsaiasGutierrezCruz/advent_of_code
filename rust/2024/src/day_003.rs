879ef42849fbf12e1eaa33e45feea528464fccf9
Isaias Gutierrez Cruz <isaiasgutierrezqfb@gmail.com>
2024-12-14 22:46:36 -0600

    feat: part 2 day 3 solved

--- rust/2024/src/day_003.rs
+++ rust/2024/src/day_003.rs
@@ -23,7 +23,6 @@ pub fn input_generator(input: &str) -> String {
 
 // Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
 
-
 #[aoc(day3, part1)]
 pub fn solve_part_01(input: &str) -> usize {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
@@ -33,21 +32,61 @@ pub fn solve_part_01(input: &str) -> usize {
     }
     results.iter().sum()
 }
+// --- Part Two ---
+// As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
 
+// There are two new instructions you'll need to handle:
+
+// The do() instruction enables future mul instructions.
+// The don't() instruction disables future mul instructions.
+// Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+// For example:
+
+// xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+// This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+// This time, the sum of the results is 48 (2*4 + 8*5).
+
+// Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+#[aoc(day3, part2)]
+pub fn solve_part_02(input: &str) -> usize {
+    let re = Regex::new(r"(do\(\))|(don't\(\))|mul\((\d+,\d+)\)").unwrap();
+    let mut results = vec![];
+    let mut multiplications_enabled = true;
+
+    for (_, [group]) in re.captures_iter(input).map(|c| c.extract()) {
+        match group {
+            "do()" => multiplications_enabled = true,
+            "don't()" => multiplications_enabled = false,
+            _ => {
+                let parts: Vec<&str> = group.split(',').collect();
+                if multiplications_enabled {
+                    results.push(
+                        parts[0].parse::<usize>().unwrap() * parts[1].parse::<usize>().unwrap(),
+                    );
+                }
+            }
+        }
+    }
+    results.iter().sum()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     const DATA: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const DATA_PART_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn sample_01() {
         assert_eq!(solve_part_01(&input_generator(DATA)), 161)
     }
 
-    // #[test]
-    // fn sample_02() {
-    //     assert_eq!(solve_part_02(&input_generator(DATA)), 4)
-    // }
+    #[test]
+    fn sample_02() {
+        assert_eq!(solve_part_02(&input_generator(DATA_PART_2)), 48)
+    }
 }
