843591f43f76d46f4c9522e3236a2f5406afc3c1
Isaias Gutierrez Cruz <isaiasgutierrezqfb@gmail.com>
2024-12-30 22:51:55 -0600

    feat: add solution to part 2 day 4

--- rust/2024/README.md
+++ rust/2024/README.md
@@ -16,4 +16,4 @@ cargo aoc
 | [Day 1: Historian Hysteria](https://github.com/IsaiasGutierrezCruz/advent_of_code/blob/main/rust/2024/src/day_001.rs) | 🌟  | 1530215 | 🌟  |26800609|
 | [Day 2: Red-Nosed Reports](https://github.com/IsaiasGutierrezCruz/advent_of_code/blob/main/rust/2024/src/day_002.rs) | 🌟  | 639 | 🌟  |674|
 | [Day 3: Mull It Over](https://github.com/IsaiasGutierrezCruz/advent_of_code/blob/main/rust/2024/src/day_003.rs) | 🌟  | 167090022 | 🌟  |89823704|
-| [Day 4: Ceres Search](https://github.com/IsaiasGutierrezCruz/advent_of_code/blob/main/rust/2024/src/day_004.rs) | 🌟  | 2358 | 🌟  |89823704|
+| [Day 4: Ceres Search](https://github.com/IsaiasGutierrezCruz/advent_of_code/blob/main/rust/2024/src/day_004.rs) | 🌟  | 2358 | 🌟  |1737|
--- rust/2024/src/day_004.rs
+++ rust/2024/src/day_004.rs
@@ -15,6 +15,13 @@ pub const DIRECTIONS: [Point; 8] = [
     Point { x: 1, y: 1 },   // up-right
 ];
 
+pub const DIAGONAL_DIRECTIONS: [Point; 4] = [
+    Point { x: 1, y: -1 },  // down-right
+    Point { x: -1, y: -1 }, // down-left
+    Point { x: -1, y: 1 },  // up-left
+    Point { x: 1, y: 1 },   // up-right
+];
+
 #[aoc_generator(day4)]
 pub fn input_generator(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
@@ -78,6 +85,26 @@ pub fn validate_pattern_in_direction(
     true
 }
 
+pub fn validate_pattern_in_direction_smaller(
+    x_limit: i32,
+    y_limit: i32,
+    point: Point,
+    direction: &Point,
+    pattern: [char; 3],
+) -> bool {
+    let steps = pattern.len() as i32 - 1;
+
+    let new_point = Point {
+        x: point.x + direction.x * steps,
+        y: point.y + direction.y * steps,
+    };
+
+    if new_point.x < 0 || new_point.x >= x_limit || new_point.y < 0 || new_point.y >= y_limit {
+        return false;
+    }
+    true
+}
+
 #[aoc(day4, part1)]
 pub fn solve_part_01(input: &Vec<Vec<char>>) -> usize {
     const PATTERN_TO_SEARCH: [char; 4] = ['X', 'M', 'A', 'S'];
@@ -125,6 +152,86 @@ pub fn solve_part_01(input: &Vec<Vec<char>>) -> usize {
     total_matches
 }
 
+// --- Part Two ---
+// The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+// Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. One way to achieve that is like this:
+
+// M.S
+// .A.
+// M.S
+// Irrelevant characters have again been replaced with . in the above diagram. Within the X, each MAS can be written forwards or backwards.
+
+// Here's the same example from before, but this time all of the X-MASes have been kept instead:
+
+// .M.S......
+// ..A..MSMS.
+// .M.S.MAA..
+// ..A.ASMSM.
+// .M.S.M....
+// ..........
+// S.S.S.S.S.
+// .A.A.A.A..
+// M.M.M.M.M.
+// ..........
+// In this example, an X-MAS appears 9 times.
+
+// Flip the word search from the instructions back over to the word search side and try again. How many times does an X-MAS appear?
+#[aoc(day4, part2)]
+pub fn solve_part_02(input: &Vec<Vec<char>>) -> usize {
+    const PATTERN_TO_SEARCH: [char; 3] = ['M', 'A', 'S'];
+    let mut total_matches = 0;
+
+    let x_size = input.len() as i32;
+    let y_size = input[0].len() as i32;
+
+    for row_index in 0..input.len() {
+        for col_index in 0..input[row_index].len() {
+            let point = Point {
+                x: row_index as i32,
+                y: col_index as i32,
+            };
+            for direction in DIAGONAL_DIRECTIONS.iter() {
+                // review if it is possible that the pattern exists in this direction
+                if !validate_pattern_in_direction_smaller(
+                    x_size,
+                    y_size,
+                    point,
+                    direction,
+                    PATTERN_TO_SEARCH,
+                ) || input[point.x as usize][point.y as usize] != PATTERN_TO_SEARCH[0]
+                {
+                    // the pattern is not possible in this direction
+                    continue;
+                }
+                // check if the pattern exists in this direction
+                let is_second_char = input[(point.x + direction.x * 1) as usize]
+                    [(point.y + direction.y * 1) as usize];
+                let is_third_char = input[(point.x + direction.x * 2) as usize]
+                    [(point.y + direction.y * 2) as usize];
+
+                // check if there is a diagonal MAS
+                let is_diagonal = (input[point.x as usize][(point.y + direction.y * 2) as usize]
+                    == PATTERN_TO_SEARCH[0]
+                    || input[(point.x + direction.x * 2) as usize][point.y as usize]
+                        == PATTERN_TO_SEARCH[0])
+                    && (input[point.x as usize][(point.y + direction.y * 2) as usize]
+                        == PATTERN_TO_SEARCH[2]
+                        || input[(point.x + direction.x * 2) as usize][point.y as usize]
+                            == PATTERN_TO_SEARCH[2]);
+
+                if is_second_char == PATTERN_TO_SEARCH[1]
+                    && is_third_char == PATTERN_TO_SEARCH[2]
+                    && is_diagonal
+                {
+                    total_matches += 1;
+                }
+            }
+        }
+    }
+    total_matches / 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -145,8 +252,8 @@ MXMXAXMASX";
         assert_eq!(solve_part_01(&input_generator(DATA)), 18)
     }
 
-    // #[test]
-    // fn sample_02() {
-    //     assert_eq!(solve_part_02(&input_generator(DATA_PART_2)), 48)
-    // }
+    #[test]
+    fn sample_02() {
+        assert_eq!(solve_part_02(&input_generator(DATA)), 9)
+    }
 }
